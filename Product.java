30834ca368b47de97622f375d883d76c3c81a2ea
Swathi H U <swathiumesh7103@gmail.com>
2025-07-18 10:30:36 +0530

    Add files via upload

---
+++ Product.java
@@ -0,0 +1,18 @@
+public class Product {
+    int productId;
+    String productName;
+    int quantity;
+    double price;
+
+    public Product(int productId, String productName, int quantity, double price) {
+        this.productId = productId;
+        this.productName = productName;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductID: " + productId + ", Name: " + productName + ", Quantity: " + quantity + ", Price: $" + price;
+    }
+}
